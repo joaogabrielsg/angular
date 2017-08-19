52a66f0dc206d12fca24af520a40500b757ba6ac
joaogabrielsg <joaogabrieldosantosgoncalves@gmail.com>
2017-08-19 01:40:20 -0300

    binding and interpolation

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -8,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
 
   url: String = 'http://loiane.com';
+  cursoAngular: boolean = true;
+
+  urlImage = 'http://lorempixel.com/400/200/nature/';
 
   constructor() { }
 
   getValor(){
     return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
   }
 
   ngOnInit() {
